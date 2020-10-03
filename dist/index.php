018f7fedf4974ae1e3d1e78f55de9b9895089e63
nadya <nadya.wyn@ya.ru>
2020-10-03 11:13:23 +0500

    Update

---
+++ dist/index.php
@@ -0,0 +1,20 @@
+<?php
+	@include "var.php";
+	@include "msqc.php";
+	@include "header.php";
+?>
+	<main class="main">
+		<?php
+			@include "dt.php";
+			@include "ht.php";
+			@include "dj.php";
+			@include "ad.php";
+			@include "ft.php";
+			@include "el.php";
+		?>
+	</main>
+	<footer class="footer"></footer>
+	<script src="js/tab.js"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>SI Test</title>
+</head>
+
+<body>
+	<?php
+		@include "security.php";
+	?>
+	Si Test Php
+</body>
+
+</html>
